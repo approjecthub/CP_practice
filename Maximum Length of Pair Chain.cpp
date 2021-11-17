68c2f78011237fed3898b7598afaa1f7c810fa72
Anirban Pal <anirbanpal1997@gmail.com>
2021-11-17 18:04:52 +0530

    TC: O(n^2)

---
+++ Maximum Length of Pair Chain.cpp
@@ -0,0 +1,33 @@
+//https://leetcode.com/problems/maximum-length-of-pair-chain/
+
+class Solution {
+public:
+    static bool customComp(vector<int> a, vector<int>b){
+        if(a[1]<b[1]){          
+            return true;
+        }
+        else if(a[1]==b[1]){
+            return a[1]<b[0];
+        }
+        else{
+            return false;
+        }
+    }
+    int findLongestChain(vector<vector<int>>& pairs) {
+        sort(pairs.begin(), pairs.end(), customComp);
+        int n = pairs.size();
+        int ans = 1;
+        for(int i=0;i<n;i++){
+            int t = pairs[i][1];
+            int c = 1;
+            for (int j=0; j<n;j++){
+                if(pairs[j][0] > t){
+                    c++;
+                    t = pairs[j][1];
+                }
+            }
+            ans = max(ans, c);
+        }
+        return ans;
+    }
+};
