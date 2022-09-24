438dec21a7ed3f1ff512300333a959d1ede009ce
approjecthub <anirbanpal1997@gmail.com>
2022-09-24 21:15:27 +0530

    triangle solved using bottom up dp, TC: O(N^2) and SC: O(N)

--- dp/triangle.cpp
+++ dp/triangle.cpp
@@ -1,5 +1,38 @@
 // https://leetcode.com/problems/triangle/
 
+// TC: O(N^2), SC: O(N)
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<int> prev(n,0);
+        vector<int> curr(n,0);
+        prev[0] = triangle[0][0];
+        
+        for(int i=1;i<n; i++){
+            for(int j=0; j<=i; j++){
+                int temp=1e9;
+                if(j<i) temp = prev[j];
+                if(j>0) temp = min(temp, prev[j-1]);
+                
+                curr[j] = triangle[i][j] + temp;
+            }
+            
+            for(int k=0; k<n; k++){
+                prev[k] = curr[k];
+            }
+        }
+        
+        int ans=INT_MAX;
+        for(int i=0; i<n; i++){
+            ans = min(ans, prev[i]);
+        }
+        
+        return ans;
+    }
+};
+
+// TC: O(N^2), SC: O(N^2)
 class Solution
 {
 public:
